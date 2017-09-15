374901cc98c9dc169fad10a8e134f0742b066eb3
Mark Oleson <fusion2004@gmail.com>
2017-09-15 11:01:44 -0400

    lots of new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,7 +13,7 @@ alias gco="git checkout"
 alias gd="git diff --color | diff-so-fancy | less --tabs=4 -RFX"
 alias gf="git fetch"
 alias gm="git merge"
-alias gr="git reset HEAD"
+alias gr="git rebase"
 alias gs="git status"
 alias gp="git pull"
 alias gu="git push"
@@ -22,11 +22,14 @@ alias a="atom ."
 alias b="bundle"
 alias f="foreman start -c web=1,lucre=1,search=1"
 alias fw="foreman start -c web=1,lucre=1,search=1,worker=1,u-messaging=1"
+alias fbg="foreman start -c web=0,lucre=1,search=1,worker=1,u-messaging=1"
+alias rs="rails s -b 0.0.0.0"
 # alias fw="forego start -c web=1,lucre=1,search=1,worker=1,u-messaging=1"
 alias fs="foreman s"
 alias r="rake"
-alias rc="rails c"
+# alias rc="rails c" # is now a function!
 alias mig="rake db:migrate" # STOP TRYING TO MAKE THIS rm - THINK ABOUT IT
+alias rt="rake test"
 alias l="LSCOLORS=gxfxcxdxbxegedabagacad ls -lahGp"
 alias nombom="npm cache clean && bower cache clean && rm -rf node_modules bower_components && npm install && bower install"
 alias nume="npm prune && npm install && bower prune && bower install"
@@ -34,6 +37,18 @@ alias prune="nume"
 alias todo="atom ~/todo"
 alias derp="ember deploy production --activate"
 alias dot="$PROJECTEDITOR $DOTFILES"
+alias barn="yarn install && yarn run bower install"
+alias yombom="rm -rf node_modules bower_components && yarn install && yarn run bower install"
+
+# Ember
+alias es="ember s"
+alias et="ember t"
+alias ets="ember t -s"
+
+# Docker
+alias dp="docker ps"
+alias db="docker start pg-develop"
+alias dc="docker-compose"
 
 alias bs="brew services"
 
