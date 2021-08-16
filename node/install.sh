cebc101089dbacea2979943a3905d0d7189454da
Mark Oleson <fusion2004@hey.com>
2021-08-16 11:19:58 -0400

    add cdk to globally installed npm packages

--- node/install.sh
+++ node/install.sh
@@ -16,4 +16,4 @@ volta install node
 
 echo
 echo "› Installing volta/node global tools..."
-volta install ember-cli yalc commitizen gitmoji-cli i18n-cloud-translator
+volta install ember-cli yalc commitizen gitmoji-cli i18n-cloud-translator aws-cdk
