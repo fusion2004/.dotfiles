c68a16fdeaa5e72c5e4c696168c1954d669376ab
Mark Oleson <fusion2004@gmail.com>
2023-11-22 17:55:51 -0500

    stop installing old ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -8,17 +8,6 @@ function install_gems() {
 eval "$(rbenv init -)"
 
 echo
-echo "› Installing ruby 2.7.8"
-rbenv install --skip-existing 2.7.8
-rbenv shell 2.7.8
-gem update --system
-
-echo
-echo "› Installing gems..."
-bundle config --global jobs 8
-gem install solargraph
-
-echo
 echo "› Installing ruby 3.2.2"
 rbenv install --skip-existing 3.2.2
 rbenv shell 3.2.2
