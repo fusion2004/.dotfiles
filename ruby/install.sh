cc4c8ced9d8016c8863a8a468e86f56eb2cccabc
Mark Oleson <mark.oleson@gusto.com>
2026-08-09 15:00:36 -0400

    hide away ruby setup for now

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,20 +5,25 @@ function install_gems() {
   gem install rubocop rubocop-rails rubocop-performance
 }
 
-eval "$(rbenv init -)"
+function activate_rbenv() {
+  eval "$(rbenv init -)"
+}
 
-echo
-echo "› Installing ruby 3.4.6"
-rbenv install --skip-existing 3.4.6
-rbenv shell 3.4.6
-gem update --system
+function setup_default_ruby() {
+  echo
+  echo "› Installing ruby 3.4.6"
+  rbenv install --skip-existing 3.4.6
+  rbenv shell 3.4.6
+  gem update --system
 
-echo
-echo "› Installing gems..."
-gem install bundler
-install_gems
+  echo
+  echo "› Installing gems..."
+  gem install bundler
+  install_gems
 
-echo
-echo "› Setting global ruby to 3.4.6"
-rbenv shell --unset
-rbenv global 3.4.6
+  echo
+  echo "› Setting global ruby to 3.4.6"
+  rbenv shell --unset
+  rbenv global 3.4.6
+}
+
